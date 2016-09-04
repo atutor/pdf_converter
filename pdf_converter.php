e71c230627c1636300e68ae27919072153b5f2ca
sudwebdesign <thomas@sudwebdesign.fr>
2016-09-04 22:52:13 +0200

    Clean && add user friendly icons (plain text or HTML2PDF)

--- pdf_converter.php
+++ pdf_converter.php
@@ -1,79 +1,38 @@
 <?php
-require('fpdf.php');
-include( 'class.ezpdf.php' );
-define('AT_INCLUDE_PATH', '../../include/');
+define('AT_INCLUDE_PATH', $_SERVER['DOCUMENT_ROOT'].'/include/');// CONTEXT_DOCUMENT_ROOT
 require (AT_INCLUDE_PATH.'vitals.inc.php');
-
 if(isset($_GET['cid'])){
 	$sql 	= "SELECT * FROM ".TABLE_PREFIX."content WHERE content_id=$_GET[cid]";
 	$result = queryDBresult($sql);
 	if($result->num_rows!=0){
 		$content_row = $result->fetch_assoc();
 		//$titulo = strip_tags($content_row['title']);
-		$titulo = $content_row['title'];
+		$titulo = '<h1 style="text-align:center;">'.$content_row['title'].'</h1>';
 		//$contenido = strip_tags($content_row['text']);
 		$contenido = $content_row['text'];
-/*
-		///JOOMLA
-		//$params = new mosParameters( $row->attribs );
-		//$params->def( 'author', 	!$mainframe->getCfg( 'hideAuthor' ) );
-		//$params->def( 'createdate', !$mainframe->getCfg( 'hideCreateDate' ) );
-		//$params->def( 'modifydate', !$mainframe->getCfg( 'hideModifyDate' ) );
-
-		$titulo   	= strip_tags(pdfCleaner( $titulo ));
-		$contenido 	= trim(strip_tags(pdfCleaner( $contenido )));
-
-
-		$pdf = new Cezpdf( 'a4', 'P' );  //A4 Portrait
-		$pdf -> ezSetCmMargins( 2, 1.5, 1, 1);
-		$pdf->selectFont( './fonts/Helvetica.afm' ); //choose font
-
-		$all = $pdf->openObject();
-		$pdf->saveState();
-		$pdf->setStrokeColor( 0, 0, 0, 1 );
-
-		// footer
-		//$pdf->addText( 250, 822, 6, $mosConfig_sitename );
-		$pdf->line( 10, 40, 578, 40 );
-		$pdf->line( 10, 818, 578, 818 );
-		//$pdf->addText( 30, 34, 6, $mosConfig_live_site );
-		//$pdf->addText( 250, 34, 6, _PDF_POWERED );
-		//$pdf->addText( 450, 34, 6, _PDF_GENERATED .' '. date( 'j F, Y, H:i', time() + $mosConfig_offset * 60 * 60 ) );
-
-		$pdf->restoreState();
-		$pdf->closeObject();
-		$pdf->addObject( $all, 'all' );
-		$pdf->ezSetDy( 30 );
-
-		$txt1 = $titulo;
-		$pdf->ezText( $txt1, 14 );
-
-		//$txt2 = AuthorDateLine( $row, $params );
-		$txt2 = $contenido;
-
-		$pdf->ezText( $txt2, 8 );
-
-		//$txt3 = $row->introtext ."\n". $row->fulltext;
-		//$pdf->ezText( $txt3, 10 );
-
-		$pdf->ezStream();
-
-		//FIN JOOMLA
-*/
-		$titulo   	= strip_tags(pdfCleaner( $titulo ));
-		$contenido 	= trim(strip_tags(pdfCleaner( $contenido )));
-
-		$pdf=new FPDF();
-		$pdf->AddPage();
-		$pdf->SetFont('Arial','',10);
-		//$pdf->Cell(0,5,$titulo);
-		//$pdf->Ln(10);
-		$pdf->MultiCell(0,3,utf8_decode($titulo.PHP_EOL.PHP_EOL.$contenido));
-		$pdf->Output();
+  
+  if (isset($_GET['html'])){
+    require(__DIR__ . '/html2pdf/vendor/autoload.php');
+    $html2pdf = new HTML2PDF('P','A4','fr');
+    $html2pdf->WriteHTML($titulo.$contenido);
+    $html2pdf->Output('exemple.pdf');
+  }
+  else{#textOnly
+    $titulo   	= strip_tags(pdfCleaner( $titulo ));
+    $contenido 	= trim(strip_tags(pdfCleaner( $contenido )));
+    require('fpdf.php');
+    include( 'class.ezpdf.php' );
+    $pdf=new FPDF();
+    $pdf->AddPage();
+    $pdf->SetFont('Arial','',10);
+    //$pdf->Cell(0,5,$titulo);
+    //$pdf->Ln(10);
+    $pdf->MultiCell(0,3,utf8_decode($titulo.PHP_EOL.PHP_EOL.$contenido));
+    $pdf->Output();
+  }
 	}
 	else{
 		echo(_AT('error'));
-
 	}
 }
 
--- side_menu.inc.php
+++ side_menu.inc.php
@@ -1,19 +1,22 @@
 <?php
-/* start output buffering: */
-ob_start();
-
 //global $savant;
 if(isset($_GET['cid'])){
+ /* start output buffering: */
+ ob_start();
+ $hrefMod ='../mods/pdf_converter/';
 	$sql 	= "SELECT * FROM ".TABLE_PREFIX."content WHERE content_id=$_GET[cid]";
 	$result = queryDBresult($sql);
 	$content_row = $result->fetch_assoc();
-	echo("<a target='_blank' href='../mods/pdf_converter/pdf_converter.php?cid=$content_row[content_id]'>$content_row[title]</a>");
+	echo("
+ <a target='_blank' href='".$hrefMod."pdf_converter.php?cid=$content_row[content_id]'><img src='".$hrefMod."Fast_text.png' alt='"._AT('pdf_converter')." "._AT('plain_text').PHP_EOL.$content_row['title']."' title='"._AT('pdf_converter')." "._AT('plain_text').PHP_EOL.$content_row['title']."' /></a>
+ <a target='_blank' href='".$hrefMod."pdf_converter.php?cid=$content_row[content_id]&amp;html=1'><img src='".$hrefMod."html_file.png' alt='"._AT('pdf_converter')." HTML2PDF".PHP_EOL.$content_row['title']."' title='"._AT('pdf_converter')." HTML2PDF".PHP_EOL.$content_row['title']."' /></a>
+ ");
 	//echo ($content_row['title']);
+ $savant->assign('dropdown_contents', ob_get_contents());
+ ob_end_clean();
+
+ $savant->assign('title', _AT('pdf_converter')); // the box title
+ $savant->display('include/box.tmpl.php');
 }
 
-$savant->assign('dropdown_contents', ob_get_contents());
-ob_end_clean();
-
-$savant->assign('title', _AT('pdf_converter')); // the box title
-$savant->display('include/box.tmpl.php');
 ?>
