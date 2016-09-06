d3fd282b31dbf476c9a825d1a345f692eeab63e8
sudwebdesign <thomas@sudwebdesign.fr>
2016-09-06 15:17:36 +0200

    Solution for generate pdf from AContent content
    Add content_type for find if an external link of AContent
    If the dom are complex, with HTML2PDF in certain cases more long to
    display a generate pdf file but finaly do the job

--- pdf_converter.php
+++ pdf_converter.php
@@ -2,22 +2,48 @@
 define('AT_INCLUDE_PATH', '../../include/');
 require (AT_INCLUDE_PATH.'vitals.inc.php');
 if(isset($_GET['cid'])){
-	$sql = "SELECT title,text FROM %scontent WHERE content_id=%d";
+	$sql = "SELECT title,text,content_type FROM %scontent WHERE content_id=%d";
 	$content_row = queryDB($sql, array(TABLE_PREFIX, $_GET['cid']),true);
 	if($content_row!=0){
 		$titulo = '<h1 style="text-align:center;">'.$content_row['title'].'</h1>';
-		$contenido = $content_row['text'];
+
+		// just for AContent content (inspired from content.php)
+		if($content_row['content_type']==2){//if(strstr($fp, 'AContentXX')){
+			$fp	= @file_get_contents($content_row['text']);
+			// a new dom object
+			$dom		= new DomDocument();
+			libxml_use_internal_errors(TRUE);
+			// load the html into the object
+			$dom->loadHTML($fp);
+			libxml_use_internal_errors(FALSE);
+			$dom->formatOutput = TRUE;
+
+			//discard white space
+			$dom->preserveWhiteSpace = false;
+
+			$node = $dom->getElementById('content-text');#4 OLD : id content-text are commented in AContent (1.4) course
+			if(empty($node));
+				$node = $dom->getElementById('content');#new id
+			if(empty($node));
+				$node = $dom->getElementById('middle-column');#Au cas ou
+   
+			$contenido	= $dom->saveXML($node, LIBXML_NOEMPTYTAG);
+			$contenido	= html_entity_decode(	$contenido );
+
+		}else{
+			$contenido = $content_row['text'];
+		}
+		$contenido = preg_replace('#<script(.*?)>(.*?)</script>#is', '', $contenido);
 
   if (isset($_GET['html'])){
-    $contenido = preg_replace('#<script(.*?)>(.*?)</script>#is', '', $contenido);
     require(__DIR__ . '/html2pdf/vendor/autoload.php');
     $html2pdf = new HTML2PDF('P','A4','fr');
     $html2pdf->setTestIsImage(false);
     $html2pdf->WriteHTML($titulo.$contenido);
     $html2pdf->Output('exemple.pdf');
   }else{#textOnly
-    $titulo   	= strip_tags(pdfCleaner( $titulo ));
-    $contenido 	= trim(strip_tags(pdfCleaner( $contenido )));
+    $titulo = pdfCleaner( $titulo );
+    $contenido = trim(pdfCleaner( $contenido ));
     require('fpdf.php');
     include( 'class.ezpdf.php' );
     $pdf=new FPDF();
@@ -32,16 +58,10 @@ if(isset($_GET['cid'])){
 	}
 }
 
-function decodeHTML( $string ) {
-	$string = strtr( $string, array_flip(get_html_translation_table( HTML_ENTITIES ) ) );
-	$string = preg_replace( "/&#([0-9]+);/me", "chr('\\1')", $string );
-	return $string;
-}
-
-
 function pdfCleaner( $text ) {
 	// Ugly but needed to get rid of all the stuff the PDF class cant handle
 
+	$text = html_entity_decode( $text );
 	$text = str_replace( '<p>', 			"\n\n", 	$text );
 	$text = str_replace( '<P>', 			"\n\n", 	$text );
 	$text = str_replace( '<br />', 			"\n", 		$text );
@@ -61,7 +81,6 @@ function pdfCleaner( $text ) {
 	$text = str_replace( '&nbsp;','',$text );
 
 	$text = strip_tags( $text );
-	$text = decodeHTML( $text );
 
 	return $text;
 }
